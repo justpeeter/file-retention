f97b8aed96cc82043686262ec8e2e26ba48b77fa
Porramet Promsang <ppromsang@Porramets-MacBook-Pro.local>
2022-05-09 15:20:05 +0700

    fail to exec find

--- backup-retention.sh
+++ backup-retention.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 
 #### Flag
 while test $# -gt 0; do
   case "$1" in
     -h|--help)
-        echo "$package - bcakup file retention with in 3 days"
+        echo "$package - backup file retention with in 3 days"
         echo " "
         echo "$package [options] application [arguments]"
         echo " "
         echo "options:"
         echo "-h, --help                        show brief help"
         echo "-d, --db-backup-path=DIR          specify a directory to store database backup file path"
-        echo "-l, --logs-backup-path=DIR        specify a directory to store transaction logs file path"
+        echo "-l, --logs-backup-path=DIR        specify a directory to store transaction logs backup file path"
         exit 0
         ;;
     -d|--db-backup-path)
@@ -19,7 +19,7 @@ while test $# -gt 0; do
         if test $# -gt 0; then
             export db_path=$1
         else
-            echo "no process specified"
+            echo "no database backup dir specified"
             exit 1
         fi
         shift
@@ -33,7 +33,7 @@ while test $# -gt 0; do
         if test $# -gt 0; then
             export logs_path=$1
         else
-            echo "no output dir specified"
+            echo "no transaction logs backup dir specified"
             exit 1
         fi
         shift
@@ -49,16 +49,26 @@ while test $# -gt 0; do
 done
 
 ### main script
-if [ -z "$db_path" ]
+if [ -z "$db_path" ] || [ ! -d "$db_path" ]
 then
-    echo "database backup file path is not specify"
+    echo "database backup file path is not specify or path $db_path does not exists." | sed "s/  / /"
 else
     echo "database backup file path is $db_path"
 fi
 
-if [ -z "$logs_path" ]
+if [ -z "$logs_path" ] || [ ! -d "$logs_path" ]
 then
-    echo "transaction logs file path is not specify"
+    echo "transaction logs backup file path is not specify or path $logs_path does not exists." | sed "s/  / /"
 else
-    echo "transaction logs file path is $logs_path"
+    echo "transaction logs backup file path is $logs_path"
+    cmd="find $logs_path -name '*.log' -type f -mtime +60" 
+    #cmd="ls"
+    echo $cmd
+    /bin/bash $cmd
+    echo "delete file successfully"
+fi
+
+if [ -z "$db_path" ] && [ -z "$logs_path" ]
+then
+    exit 1
 fi
