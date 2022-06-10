2f838d375aa02673cec459a77cb6481b14f050ad
Porramet Promsang <ppromsang@Porramets-MacBook-Pro.local>
2022-06-10 21:25:30 +0700

    add logs file

--- file-retention.sh
+++ file-retention.sh
@@ -9,11 +9,12 @@ while test $# -gt 0; do
         echo "file-retention.sh [options] application [arguments]"
         echo " "
         echo "options:"
-        echo "-h, --help                        show brief help"
-        echo "-db, --db-backup-path=DIR         specify a directory to store database backup file path"
-        echo "-logs, --logs-backup-path=DIR     specify a directory to store transaction logs backup file path"
-        echo "-hana, --hana-backup-path=DIR     specify a directory to store hana backup file path"
-        echo "--days=DIR                        specify day retention period expires.(default=3)"
+        echo "-h, --help                            show brief help"
+        echo "-db, --db-backup-path=DIR             specify a directory to store database backup file path"
+        echo "-logs, --logs-backup-path=DIR         specify a directory to store transaction logs backup file path"
+        echo "-hana, --hana-backup-path=DIR         specify a directory to store hana backup file path"
+        echo "--days=DIR                            specify day retention period expires.(default=3)"
+        echo "-display, --display-logs-path=DIR     specify a directory to store script log file path(default=/otp/DB_Retention)"
         exit 0
         ;;
     -db|--db-backup-path)
@@ -72,9 +73,6 @@ while test $# -gt 0; do
         export days=`echo $1 | sed -e 's/^[^=]*=//g'`
         shift
         ;;
-    *)
-        break
-        ;;
     -display|--display-logs-path)
         shift
         if test $# -gt 0; then
@@ -88,6 +86,9 @@ while test $# -gt 0; do
     --display-logs-path*)
         export default_path=`echo $1 | sed -e 's/^[^=]*=//g'`
         shift
+        ;;
+    *)
+        break
         ;;
   esac
 done
@@ -104,36 +105,38 @@ then
     default_path="/opt/DB_Retention"
 fi
 
-if [ ! -d "$default_path" ]
+if [ ! -d "$default_path/logs" ]
 then
-    mkdir -p "$default_path"
+    mkdir -p "$default_path/logs"
 fi
+in_date=$(date +"%Y%m%d")
+default_path="$default_path/logs"
 
 if [ -z "$db_path" ] || [ ! -d "$db_path" ]
 then
-    echo "database backup file path is not specify or path $db_path does not exists." | sed "s/  / /" >> "$default_path"/retention.log
+    echo "database backup file path is not specify or path $db_path does not exists." | sed "s/  / /" >> "$default_path/$in_date.retention.log"
 else
-    echo "database backup file path is $db_path." >> "$default_path"/retention.log
-    find "$db_path" -maxdepth 3 -name "*.DB.*"  -type f -mmin +$(($days*24*60)) -print -delete >> "$default_path"/retention.log
-    echo "delete database backup file successfully." >> "$default_path"/retention.log
+    echo "database backup file path is $db_path." >> "$default_path/$in_date.retention.log"
+    find "$db_path" -maxdepth 3 -name "*.DB.*"  -type f -mmin +$(($days*24*60)) -print -delete >> "$default_path/$in_date.retention.log"
+    echo "delete database backup file successfully." >> "$default_path/$in_date.retention.log"
 fi
 
 if [ -z "$logs_path" ] || [ ! -d "$logs_path" ]
 then
-    echo "transaction logs backup file path is not specify or path $logs_path does not exists." | sed "s/  / /" >> "$default_path"/retention.log
+    echo "transaction logs backup file path is not specify or path $logs_path does not exists." | sed "s/  / /" >> "$default_path/$in_date.retention.log"
 else
-    echo "transaction logs backup file path is $logs_path." >> "$default_path"/retention.log
-    find "$logs_path" -maxdepth 3 -name "*.TRAN.*"  -type f -mmin +$(($days*24*60)) -print -delete >> "$default_path"/retention.log
-    echo "delete transaction logs backup file successfully." >> "$default_path"/retention.log
+    echo "transaction logs backup file path is $logs_path." >> "$default_path/$in_date.retention.log"
+    find "$logs_path" -maxdepth 3 -name "*.TRAN.*"  -type f -mmin +$(($days*24*60)) -print -delete >> "$default_path/$in_date.retention.log"
+    echo "delete transaction logs backup file successfully." >> "$default_path/$in_date.retention.log"
 fi
 
 if [ -z "$hana_path" ] || [ ! -d "$hana_path" ]
 then
-    echo "hana backup file path is not specify or path $hana_path does not exists." | sed "s/  / /" >> "$default_path"/retention.log
+    echo "hana backup file path is not specify or path $hana_path does not exists." | sed "s/  / /" >> "$default_path/$in_date.retention.log"
 else
-    echo "hana backup file path is $hana_path." >> "$default_path"/retention.log
-    find "$hana_path" -maxdepth 3 -name "*_databackup_*"  -type f -mmin +$(($days*24*60)) -print -delete >> "$default_path"/retention.log
-    echo "delete hana backup file successfully." >> "$default_path"/retention.log
+    echo "hana backup file path is $hana_path." >> "$default_path/$in_date.retention.log"
+    find "$hana_path" -maxdepth 3 -name "*_databackup_*"  -type f -mmin +$(($days*24*60)) -print -delete >> "$default_path/$in_date.retention.log"
+    echo "delete hana backup file successfully." >> "$default_path/$in_date.retention.log"
 fi
 
 if [ -z "$db_path" ] && [ -z "$logs_path" ] && [ -z "$hana_path" ]
